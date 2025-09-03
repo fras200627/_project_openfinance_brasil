c0c047aa67840a3de4b3264e1ba79400f73d8a68
fras200627 <fras200627@gmail.com>
2025-09-03 13:59:14 -0300

    + revision documents + start orchs

--- workspace/02.2-authorization.server/sboot-orch-ofb-authorization-server/src/main/java/com/ofb/authorization/repository/RegisteredClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ofb.authorization.repository;
-
-import com.ofb.authorization.model.RegisteredClientEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RegisteredClientRepository extends JpaRepository<RegisteredClientEntity, String> {
-    Optional<RegisteredClientEntity>  findByClientId(String clientId);
-}
